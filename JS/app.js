6ddb1b6373ab2d5e638cbd3adf034f4274c05a07
Ildi Dvorani <dvoranii@ildidev.com>
2022-06-22 17:36:08 -0400

    completed desktop view of hero section, started services section

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,30 @@
+// Typing Effect
+function typeEffect(element, speed) {
+  let text = "Front-End Developer";
+
+  let i = 0;
+
+  let timer = setInterval(() => {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+let speed = 75;
+let jobTitle = document.querySelector(".hero-job-title-text");
+
 // nav
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const navList = document.querySelector(".nav-list");
+let delay = jobTitle.innerHTML.length * speed + speed;
+
+setTimeout(() => {
+  typeEffect(jobTitle, speed);
+}, 300);
 
 btnNavEl.addEventListener("click", () => {
   navList.classList.toggle("nav-active");
