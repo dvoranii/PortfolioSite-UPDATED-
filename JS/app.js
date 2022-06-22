40e0ebb09c123dc2bdd84c70b54357d5ff06f705
Ildi Dvorani <dvoranii@ildidev.com>
2022-06-22 18:49:30 -0400

    added background texture to body element and fixed scrolling/header bug

--- JS/app.js
+++ JS/app.js
@@ -29,3 +29,28 @@ setTimeout(() => {
 btnNavEl.addEventListener("click", () => {
   navList.classList.toggle("nav-active");
 });
+
+// Sticky Nav
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+      document.body.classList.add("box-shadow");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+      document.body.classList.remove("box-shadow");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-96px",
+  }
+);
+obs.observe(sectionHeroEl);
