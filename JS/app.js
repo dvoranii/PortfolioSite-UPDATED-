53dab18ea2c1ebee3c685e1e3b06eb6bb109cc01
Ildi Dvorani <dvoranii@ildidev.com>
2022-06-27 12:16:34 -0400

    created projects model and implemented template programmatically

--- JS/app.js
+++ JS/app.js
@@ -71,6 +71,87 @@ obs.observe(sectionHeroEl);
 
 // instance.start();
 
+// Fetch modal
+
+let htmlTemplate;
+let modalContainer = document.querySelector(".modal");
+
+const getModal = (container, title) => {
+  fetch("/models/projects.json")
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let projArr = Object.entries(data);
+
+      projArr.forEach((project, i, arr) => {
+        // console.log(project, i, arr);
+        console.log(project[1].title);
+        htmlTemplate = `
+        <h2 class="project-title mobile">${project[1].title}</h2>
+        <div class="modal-img-wrapper">
+            <img src="${project[1].images.currentImg}" alt="" class="modal-img current-img">
+
+                <div class="gallery-img-wrapper">
+                    <img src="${project[1].images.imgGallery.item1}" id="gallery-item" alt="">
+                    <img src="${project[1].images.imgGallery.item2}" id="gallery-item" alt="">
+                    <img src="${project[1].images.imgGallery.item3}" id="gallery-item" alt="">
+                    <img src="${project[1].images.imgGallery.item4}" id="gallery-item" alt="">
+                </div>
+            </div>
+        <div class="project-details">
+            <div class="project-header">
+                <h2 class="project-title desktop">${project[1].title}</h2>
+            </div>
+            <p class="modal-paragraph">${project[1].description}</p>
+
+                <div class="tech-icons">
+                    <ul class="tech-icons-list">
+                        <li class="tech-item tech-item-1"><img src="${project[1].techIcons.icon1}" alt=""></li>
+                        <li class="tech-item tech-item-1"><img src="${project[1].techIcons.icon2}" alt=""></li>
+                        <li class="tech-item tech-item-1"><img src="${project[1].techIcons.icon3}" alt=""></li>
+                        <li class="tech-item tech-item-1"><img src="${project[1].techIcons.icon4}" alt=""></li>
+                        <li class="tech-item tech-item-1"><img src="${project[1].techIcons.icon5}" alt=""></li>
+                        <li class="tech-item tech-item-1"><img src="${project[1].techIcons.icon6}" alt=""></li>
+                    </ul>
+                </div>
+
+                <div class="modal-btns">
+                    <a href ="${project[1].btnLinks.liveSite}" target="_blank"">
+                        <button class="live-demo-btn">
+                        <div class="icon-wrapper">
+                            <img src="/assets/modal-img/laptop-outline.svg" alt="" id="modal-laptop-icon">
+                            <span class="live-demo-text">Live Website</span>
+                        </div>
+                    </button>
+                    </a>
+
+                    <a href="${project[1].btnLinks.repo}" target="_blank">
+                        <button class="gh-btn">
+                            <div class="icon-wrapper">
+                                <img src="/assets/modal-img//logo-github (1).svg" alt="" id="modal-gh-icon">
+                                <span class="gh-text">Repository</span>
+                            </div>
+                        </button>
+                    </a>
+                 </div>
+
+        </div>
+        `;
+
+        if (project[1].title === title) {
+          container.insertAdjacentHTML("beforeend", htmlTemplate);
+        } else {
+          return;
+        }
+      });
+    });
+};
+
+getModal(modalContainer, "OG Creations Recording Studio Website");
+
 const textBox = document.querySelectorAll(".textbox");
 const item = document.querySelectorAll(".item");
 const imgBox = document.querySelector(".image-box");
@@ -119,13 +200,15 @@ closeBtn.addEventListener("click", () => {
 let galleryItem = document.querySelectorAll("#gallery-item");
 let currentImg = document.querySelector(".current-img");
 
-let placeholderSrc;
+let placeholder;
 
 galleryItem.forEach((item) => {
   item.addEventListener("click", (e) => {
-    placeholderSrc = e.currentTarget.src;
+    e.preventDefault();
+
+    placeholder = e.currentTarget.src;
     e.currentTarget.src = currentImg.src;
-    currentImg.src = placeholderSrc;
+    currentImg.src = placeholder;
   });
 });
 
